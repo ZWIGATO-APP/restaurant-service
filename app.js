99e9ec3d36f0e43e2558267f7fed14478e3cb314
Monika2399 <moni.selva99@gmail.com>
2024-11-15 22:36:11 +0530

    changed port number

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/restaurants', restaurantRoutes);
 
 // Start the server if not in test mode
 // if (process.env.NODE_ENV !== 'test') {
-    const PORT = process.env.PORT || 3001;
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
